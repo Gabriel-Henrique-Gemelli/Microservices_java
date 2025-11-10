e382f33b21bdd354f1eb322eb045c166bf2b7479
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2025-11-10 14:40:00 -0300

    API funcionando via DOCKER 😍

--- gateway-service/src/main/java/br/edu/atitus/gateway_service/ApigatewayConfig.java
+++ gateway-service/src/main/java/br/edu/atitus/gateway_service/ApigatewayConfig.java
@@ -13,35 +13,27 @@ public class ApigatewayConfig {
         return builder.routes()
             .route(p -> p
                 .path("/get")
-                .filters(f -> f.addRequestHeader("X-USER-NAME", "username").addRequestParameter("name", "fulano"))
                 .uri("http://httpbin.org:80"))
             .route(p -> p
                 .path("/product/**")
-                .filters(f -> f.rewritePath("/product/(?<segment>.*)", "/${segment}"))
                 .uri("lb://product-service"))
             .route(p -> p
                 .path("/currency/**")
-                .filters(f -> f.rewritePath("/currency/(?<segment>.*)", "/${segment}"))
                 .uri("lb://currency-service"))
             .route(p -> p
                 .path("/auth/**")
-                .filters(f -> f.rewritePath("/auth/(?<segment>.*)", "/${segment}"))
                 .uri("lb://auth-service"))
             .route(p -> p
                 .path("/orders/**")
-                .filters(f -> f.rewritePath("/orders/(?<segment>.*)", "/${segment}"))
                 .uri("lb://order-service"))
             .route(p -> p
                 .path("/ws/orders/**")
-                .filters(f -> f.rewritePath("/ws/orders/(?<segment>.*)", "/${segment}"))
                 .uri("lb:ws://order-service"))
             .route(p -> p
                 .path("/greeting/**")
-                .filters(f -> f.rewritePath("/greeting/(?<segment>.*)", "/${segment}"))
                 .uri("lb://greeting-service"))
             .route(p -> p
                 .path("/cart/**")
-                .filters(f -> f.rewritePath("/cart/(?<segment>.*)", "/${segment}"))
                 .uri("lb://cart-service"))
             .build();
     }
